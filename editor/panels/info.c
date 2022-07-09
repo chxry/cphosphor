c9d49b49f4402a8b3e885e21424ef04b778f8ee1
chxry <moosejuicetex@gmail.com>
2022-07-09 15:55:39 +0100

    acknowledgements

--- editor/panels/info.c
+++ editor/panels/info.c
@@ -32,6 +32,7 @@ void info_render() {
       igSameLine(size.x - 8, 0);
       igText("v" VERSION);
       igSeparator();
+      igPushTextWrapPos(size.x);
       if (igTreeNode_Str("Debug Info")) {
         if (igTreeNode_Str("World")) {
           igText("Entities: %i", world.entities.length);
@@ -52,10 +53,53 @@ void info_render() {
           igText("%s", glGetString(GL_RENDERER));
           igTreePop();
         }
-
+        igTreePop();
+      }
+      if (igTreeNode_Str("Acknowledgments")) {
+        if (igTreeNode_Str("Simple Directmedia Layer")) {
+          igTextWrapped("Copyright (C) 1997-2022 Sam Lantinga <slouken@libsdl.org>\n\n" ZLIB_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("Lua")) {
+          igTextWrapped("Copyright © 1994–2022 Lua.org, PUC-Rio.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("PhysicsFS")) {
+          igTextWrapped("Copyright (c) 2001-2022 Ryan C. Gordon <icculus@icculus.org> and others.\n\n" ZLIB_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("glad")) {
+          igTextWrapped("Copyright (c) 2013-2022 David Herberth.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("cimgui")) {
+          igTextWrapped("Copyright (c) 2015 Stephan Dilly.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("Dear ImGui")) {
+          igTextWrapped("Copyright (c) 2014-2022 Omar Cornut.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("cglm")) {
+          igTextWrapped("Copyright (c) 2015 Recep Aslantas <info@recp.me>.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("tinyobjloader")) {
+          igTextWrapped("Copyright (c) 2012-2019 Syoyo Fujita and many contributors.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("vec, map & cmixer")) {
+          igTextWrapped("Copyright (c) 2014,2017 rxi.\n\n" MIT_LICENSE);
+          igTreePop();
+        }
+        if (igTreeNode_Str("IconFontCppHeaders")) {
+          igTextWrapped("Copyright (c) 2017 Juliette Foucaut and Doug Binks.\n\n" ZLIB_LICENSE);
+          igTreePop();
+        }
         igTreePop();
       }
     }
+    igPopTextWrapPos();
     igEnd();
   }
   igPopStyleVar(1);
